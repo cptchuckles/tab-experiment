8256beb170401b6c19c50433ba98f42d42cd747a
jcgollnick <jonathan.gollnick@gmail.com>
2019-10-26 20:35:43 -0500

    Create content divs and show/hide function

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Click to select tab
 
+const contents = document.querySelectorAll( ".content" );
+
 const sidebarItems = document.querySelectorAll("#sidebar>ul>li");
+
 sidebarItems.forEach(li => {
   li.addEventListener("click", e => {
     sidebarItems.forEach(l => l.classList.remove("selected"));
-    e.target.classList.add("selected");
+    e.target.classList.add( "selected" );
+    showRelatedContentTab( e.target );
   });
 });
+
+function showRelatedContentTab( tab ) {
+  const contentName = tab.getAttribute( "data" );
+  if( ! contentName ) return;
+  
+  contents.forEach( content => {
+    if( content.id == contentName ) {
+      content.style.visibility = "visible";
+    } else {
+      content.style.visibility = "hidden";
+    }
+  });
+}
